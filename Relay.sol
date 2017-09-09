23744cf4a4c3a8a0daa66531fbb062d8b4b16a68
VK <kedia.vikas@gmail.com>
2017-09-09 04:03:43 -0700

    throw is deprecated

--- Relay.sol
+++ Relay.sol
@@ -9,13 +9,13 @@ contract Relay {
     }
 
     function update(address newAddress){
-        if(msg.sender != 0x6a35399974126a6d739D3FA139AE64E32303DF8a) throw; // only the owner can call to update the value of current version.
+        require(msg.sender == 0x6a35399974126a6d739D3FA139AE64E32303DF8a); // only the owner can call to update the value of current version.
         currentVersion = newAddress;
     }
 
     // DELEGATECALL basically says that I'm a contract and I'm allowing (delegating) you to do whatever you want to my storage
     // ref: https://ethereum.stackexchange.com/questions/3667/difference-between-call-callcode-and-delegatecall
     function(){
-        if(!currentVersion.delegatecall(msg.data)) throw;
+        require(currentVersion.delegatecall(msg.data));
     }
 }
