1d28d26a7f8e5e6c1199959bbeaac8270666a3db
Pavel Rubin <pash7ka@gmail.com>
2017-10-04 15:09:05 +0300

    Fix possible out of gas in fallback method: use commit() to transfer collected ether

--- contracts/PersonalCrowdsale.sol
+++ contracts/PersonalCrowdsale.sol
@@ -17,9 +17,14 @@ contract PersonalCrowdsale {
 
   function() payable {
     require(!claimed);  //if tokens were claimed then accessKey is stored on blockhain, so we don't allow to buy any more tokens with this accessKey
-    sale.saleTo.value(msg.value)(this, partner);
+    //sale.saleTo.value(msg.value)(this, partner);  //Only 2300 gas may be available here, not enought to send. Use commit() instead
   }
 
+  /**
+  * @notice Send tokens to a specified address
+  * @param to Where to send tokens
+  * @param accessKey Key to proove you are the owner of tokens
+  */
   function claim(address to, string accessKey) returns(bool){
     bytes32 hash = sha3(accessKey);
     if(hash != accessKeyHash){
@@ -32,10 +37,23 @@ contract PersonalCrowdsale {
         return true;
     }
   }
+  /**
+  * @notice How much tokens available to claim
+  */
   function balance() public constant returns(uint256){
         HealthToken hlt = sale.hlt();
         uint256 _balance = hlt.balanceOf(this);
         return _balance;
   }
+  /**
+  * @notice Send colected ether to the Crowdsale to buy tokens
+  * @dev Crowdsale server should call this, but anyone can
+  */
+  function commit() public {
+    if(address(this).balance > 0){
+        sale.saleTo.value(address(this).balance)(this, partner);
+    }
+  }
+
 }
 
